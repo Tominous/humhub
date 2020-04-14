cb1ae61ae4bdfd17886cc5bfb1d02913ca4807e4
Tominous <18189490+Tominous@users.noreply.github.com>
2020-04-14 14:01:04 +0100

    Update profileHeaderImageUpload.js

--- static/resources/user/profileHeaderImageUpload.js
+++ static/resources/user/profileHeaderImageUpload.js
@@ -17,7 +17,7 @@ $(document).on('humhub:ready', function () {
                 url: profileImageUploaderUrl,
                 dataType: 'json',
                 singleFileUploads: true,
-                //formData: {'CSRF_TOKEN': csrfValue},
+                formData: {'CSRF_TOKEN': csrfValue},
                 limitMultiFileUploads: 1,
                 progressall: function (e, data) {
                     var progress = parseInt(data.loaded / data.total * 100, 10);
@@ -59,7 +59,7 @@ $(document).on('humhub:ready', function () {
                 url: profileHeaderUploaderUrl,
                 dataType: 'json',
                 singleFileUploads: true,
-                //formData: {'CSRF_TOKEN': csrfValue},
+                formData: {'CSRF_TOKEN': csrfValue},
                 limitMultiFileUploads: 1,
                 progressall: function (e, data) {
                     var progress = parseInt(data.loaded / data.total * 100, 10);
